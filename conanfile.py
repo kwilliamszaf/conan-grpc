4bc8a48487788903a44179b52ece3d52f414459e
Kevin Williams <kevin.williams@vastech.co.za>
2020-04-28 09:48:49 +0200

    added v1.29.0

--- conanfile.py
+++ conanfile.py
@@ -49,7 +49,7 @@ class grpcConan(ConanFile):
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
         extracted_dir = "1.29.0"
-        tools.mkdir("test")
+        tools.mkdir("kw")
         os.rename(extracted_dir, self._source_subfolder)
 
         cmake_path = os.path.join(self._source_subfolder, "CMakeLists.txt")
