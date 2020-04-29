4a642b5d73b9e15f6599819c9b1c9cc15f84fd5c
Kevin Williams <kevin.williams@vastech.co.za>
2020-04-29 09:21:14 +0200

    .

--- conanfile.py
+++ conanfile.py
@@ -46,7 +46,7 @@ class grpcConan(ConanFile):
         "zlib/1.2.11",
         "openssl/1.1.1d",
         "protobuf/3.9.1@bincrafters/stable",
-        "protoc_installer/3.11.4@bincrafters/stable",
+        "protoc_installer/3.9.1@bincrafters/stable",
         "c-ares/1.15.0"
     )
 
