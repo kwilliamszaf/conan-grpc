329a7484fc41f8a8fcdbeab920d7fe172c9c1dec
Kevin Williams <kevin.williams@vastech.co.za>
2020-05-05 16:30:43 +0200

    trying a build step

--- conanfile.py
+++ conanfile.py
@@ -110,7 +110,7 @@ class grpcConan(ConanFile):
         return self._cmake
 
     def build(self):
-	    self.run("mkdir build")
+        self.run("mkdir build")
         self.run("cd build")
         self.run("cmake build .. -DCMAKE_BUILD_TYPE=Release -DgRPC_PROTOBUF_PROVIDER=package")
         self.run("activate_run.bat")
